7447a2f6fa00810f46c0309a9ef640fb500f0c4d
Jason <jasonyata@gmail.com>
2020-01-06 15:25:07 -0800

    lots o stuff

--- Archi HTML (dark)/js/owl.carousel.js
+++ Archi HTML (dark)/js/owl.carousel.js
@@ -1465,8 +1465,8 @@ if (typeof Object.create !== "function") {
         paginationSpeed : 800,
         rewindSpeed : 1000,
 
-        autoPlay : false,
-        stopOnHover : false,
+        autoPlay : true,
+        stopOnHover : true,
 
         navigation : false,
         navigationText : ["prev", "next"],
