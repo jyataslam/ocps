6924ce37bbc0da6a6d14457d91a8cda59150da52
Davideuijinlee <Davideuijinlee@gmail.com>
2020-02-13 11:11:40 -0800

    fixed jasons shitty function

--- js/test.js
+++ js/test.js
@@ -1,5 +1,7 @@
 $(document).ready(
     createContentful()
+
+
 )
 
 
@@ -17,11 +19,13 @@ const patientInfo = {
     patientDesc: ''
 }
 
+let contentID = '';
+
 const patientArray = [];
 
 async function createContentful() {
-    grabData();
-    // await createPatientInfo();
+    await grabData();
+
 }
 
 
@@ -45,8 +49,10 @@ function grabData() {
                 patientInfo.patientWeight = entry.fields.weight;
                 patientInfo.patientID = entry.fields.id;
                 patientInfo.patientPhotosAll = entry.fields.photos;
-                patientInfo.patientDetails = entry.fields.details;
+                patientInfo.patientDetails = entry.fields.details.replace(//g,"•").split('/[•,\/ ]/');
                 patientInfo.patientDesc = entry.fields.patientDescription;
+
+                getSpecificEntry();
             })
     } else {
         const pageTitle = $('.header-title').text();
@@ -57,7 +63,7 @@ function grabData() {
         }).then(function (entries) {
             entries.items.forEach(function (entry) {
 
-                let patientLiteral = 
+                let patientLiteral =
                     `<div class='col-xs-12 col-sm-6 col-md-4 photos-col wow fadeInUp animated' wow-delay='0s'>
         <div class='text-middle'>
             <div class='twentytwenty-container'>
@@ -72,66 +78,158 @@ function grabData() {
             <div class="photos-content-flex-row">
                 <div class="photos-content-left">
                     <p class="photos-age">Age: 
-                        <span class="main-photos-age-span">${entry.fields.age}</span>
+                        <span class="main-photos-age-span">Age: ${entry.fields.age}</span>
                     </p>
                     <p class="photos-gender">Gender: 
-                        <span class="main-photos-gender-span">${entry.fields.gender}</span>
+                        <span class="main-photos-gender-span">Gender: ${entry.fields.gender}</span>
                     </p>
                     <p class="photos-ethnicity">Ethnicity: 
-                        <span class="main-photos-ethnicity-span">${entry.fields.ethnicity}</span>
+                        <span class="main-photos-ethnicity-span">Ethnicity: ${entry.fields.ethnicity}</span>
                     </p>
                 </div>
                 <div class="photos-content-right">
                     <p class="photos-height">Height: 
-                        <span class="main-photos-height-span">${entry.fields.height}</span>
+                        <span class="main-photos-height-span">Height: ${entry.fields.height}</span>
                     </p>
                     <p class="photos-weight">Weight: 
-                        <span class="main-photos-weight-span">${entry.fields.height}</span>
+                        <span class="main-photos-weight-span">Weight: ${entry.fields.weight}</span>
                     </p>
                 </div>
             </div>
-            <a class='btn-line facelift-url-btn'>View Details</a>
+            <a class='btn-line facelift-url-btn' id='${entry.sys.id}'>View Details</a>
         </div>
     </div>`
 
-                patientArray.push(patientLiteral);
+                contentID = entry.sys.id;
 
+                patientArray.push(patientLiteral);
             })
+
             $('.main-all-photos-page').append(patientArray);
 
+            $('.facelift-url-btn').on('click', function () {
+                console.log('clicked')
+
+
+                window.open(window.location.href + '#' + contentID);
+            })
         })
 
     }
+
 }
 
+function splitArrayByGroups(inputArr, splitNum) {
+    var index = 0;
+    var arrayLength = inputArr.length;
+    var tempArr = [];
+
+    for (index = 0; index < arrayLength; index += splitNum) {
+        newArr = inputArr.slice(index, index + splitNum);
+        tempArr.push(newArr);
+    }
+
+    return tempArr;
+}
+
+function getSpecificEntry() {
+
+    let specificPatientInfo =
+        `<div class='col-xs-12'>
+    <div class="text-middle photos-content-wrapper">
+        <h3>
+            <span class="id-color photos-header">Client Details</span>
+        </h3>
+        <p class="client-bio">${patientInfo.patientDesc}</p>
+    </div>
+</div>
+<div class="col-xs-12 col-sm-6">
+    <div class="expand-group individual-photos-dropdown">
+        <div class="expand">
+            <h4 class="individual-photos-header expand_patient_info">Patient Information</h4>
+            <div class="hidden-content patient-hidden-content">
+                <p class="photos-age">
+                    <span class="photos-age-span">Age: ${patientInfo.patientAge}</span>
+                </p>
+                <p class="photos-gender">
+                    <span class="photos-gender-span">Gender: ${patientInfo.patientGender}</span>
+                </p>
+                <p class="photos-ethnicity">
+                    <span class="photos-ethnicity-span">Ethnicity: ${patientInfo.patientEthnicity}</span>
+                </p>
+                <p class="photos-height">
+                    <span class="photos-height-span">Height: ${patientInfo.patientHeight}</span>
+                </p>
+                <p class="photos-weight">
+                    <span class="photos-weight-span">Weight: ${patientInfo.patientWeight}</span>
+                </p>
+            </div>
+        </div>
+        <div class="expand">
+            <h4 class="individual-photos-header expand_procedure">Procedure Details</h4>
+            <div class="hidden-content hidden-procedures-loop">
+                <p class="individual-photos-procedure-content">${patientInfo.patientDetails}</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-xs-12 col-sm-6 individual-profile-wrapper">
+    <div class="individual-profile-details-container">
+        <p class="individual-profile-name">Dr. Bunkis</p>
+        <p class="individual-profile-title">Medical Director</p>
+        <p class="individual-profile-phone">
+            <a href="tel:+19498889700">949.888.9700</a>
+        </p>
+    </div>
+    <div class="individual-profile-photo-container"">
+        <img src=" ../images-bunkis/dr-bunkis-headshot.jpg" alt="">
+    </div>
+</div>
+`
 
 
-// function createPatientInfo () {
-//     if(patientInfo.containsHash){
-//         let patientInfo 
-//     } else {
-//         let patientArray 
-//     }
-// }
-    // var client = contentful.createClient({
-    //     space: "szbt0zwjreyq",
-    //     accessToken: "qL-R79I6HSdp5nvptoGUAgFqTVSU49kAOfZA3-zT2CU"
-    // });
 
-    // if (window.location.hash) {
 
-    //     patientInfo.hash = window.location.hash.substring(1);
 
-    //     client.getEntry(hash)
-    //         .then(function (entry) {
-    //             patientProcedure = entry.fields.procedure;
-    //             patientAge = entry.fields.age;
-    //             patientGender = entry.fields.gender;
-    //             patientEthnicity = entry.fields.ethnicity;
-    //             patientHeight = entry.fields.height;
-    //             patientWeight = entry.fields.weight;
-    //             patientID = entry.fields.id;
-    //             patientPhotosAll = entry.fields.photos;
-    //             patientDetails = entry.fields.details;
-    //             patientDesc = entry.fields.patientDescription;
-    //         
+    $('.main-all-photos-page').append(specificPatientInfo)
+
+
+    console.log('patient photos #2', patientInfo.patientPhotosAll);
+    patientInfo.patientPhotosAll.forEach(item => {
+        patientArray.push(item.fields.file.url)
+    })
+
+    const resultArr = splitArrayByGroups(patientArray, 2);
+
+    for (i = 0; i < resultArr.length; i++) {
+
+        let specificPatient =
+            `<div class="col-xs-12 col-md-6 col-md-offset-3 photos-col">
+            <div class="text-middle">
+                <div class="twentytwenty-container">
+                <img alt="dr bunkis before after photo"
+                    class="img-responsive individual-image-100-width twentytwenty-before"
+                    src=${resultArr[i][0]}>
+                <img alt="dr bunkis before after photo"
+                    class="img-responsive individual-image-100-width twentytwenty-before"
+                    src=${resultArr[i][1]}>
+                </div>
+            </div>
+        </div>`
+
+        $('.main-all-photos-page').append(specificPatient)
+    }
+
+    $('.main-all-photos-page').addClass('individual-photos-bg-white');
+
+    $('.expand_patient_info').click(() => {
+        $('.patient-hidden-content').toggleClass('hidden-content-active');
+      })
+
+      $('.expand_procedure').click(() => {
+        $('.hidden-procedures-loop').toggleClass('hidden-content-active');
+      })
+
+
+}
+
