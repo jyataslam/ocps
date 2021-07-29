aafe2a6e142be44574461105b45d30ec0e095138
Jason <jasonyata@gmail.com>
2021-07-28 23:05:14 -0700

    added popup on homepage load for special

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+// debugger;
+// if (localStorage.getItem("modal") === "true") {
+//     $("landing-modal").css("display", "none");
+// } else if (localStorage.getItem("modal") === null) {
+//     $("#landing-modal").modal({
+//         fadeDuration: 500,
+//     });
+//     localStorage.setItem("modal", "true");
+// }
+
+$("#landing-modal").modal({
+    fadeDuration: 500,
+});
