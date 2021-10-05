366f34476821637847fe804554992bd56600d1dd
Mymy <thoaimydinh@gmail.com>
2021-10-05 14:52:35 -0700

    modal special

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,12 @@
 // debugger;
-// if (localStorage.getItem("modal") === "true") {
-//     $("landing-modal").css("display", "none");
-// } else if (localStorage.getItem("modal") === null) {
-//     $("#landing-modal").modal({
-//         fadeDuration: 500,
-//     });
-//     localStorage.setItem("modal", "true");
-// }
+if (localStorage.getItem("modal") === "true") {
+    $("landing-modal").css("display", "none");
+} else if (localStorage.getItem("modal") === null) {
+    $("#landing-modal").modal({
+        fadeDuration: 500,
+    });
+    localStorage.setItem("modal", "true");
+}
 
 $("#landing-modal").modal({
     fadeDuration: 500,
